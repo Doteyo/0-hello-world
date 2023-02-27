cbec3ed05584cc0b2764671c316c26e8a90eec99
Pavel <koolersxs@gmail.com>
2023-02-27 13:25:47 +0500

    UPD

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-console.log("hello world!");
-console.log("Hello u 2!");
+console.log("hello world!")
+console.log("Hello u 2!")
+var дадаад = 1
+$ = "s"
+console.log(дадаад)
+let UserAge;
+const a = '12'; const b = '4'; const c = '2';
